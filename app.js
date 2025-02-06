e9291103e2f66a460ad40d9abdaa039b6baf4158
coderavvi <iamtabson@gmail.com>
2025-02-06 10:52:16 +0100

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import cors from "cors";
 import axios from "axios";
+import compression from "compression";
 
 // importing helper functions
 import {
@@ -11,27 +12,23 @@ import {
   sumOfDigits,
 } from "./util.js";
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-
-
-
+app.use(compression());
 
 // Define the get route for the API
 app.get("/api/classify-number?:number", async (req, res) => {
   const number = Number(req.query.number);
 
   // user input validation
-  if (!number || isNaN(number) || !Number.isInteger(number) || number==='') {
+  if (!number || isNaN(number) || !Number.isInteger(number) || number === "") {
     return res.status(400).json({
       number: req.query.number,
       error: true,
     });
   }
-
 
   const numberProperties = [];
 
@@ -45,28 +42,24 @@ app.get("/api/classify-number?:number", async (req, res) => {
     numberProperties.push("odd"); // adds 'odd' to numberProperties array if true
   }
 
-
   // getting fun fact from the numbersapi
   try {
     const response = await axios.get(`http://numbersapi.com/${number}/math`);
     const funFact = response.data;
 
     const resData = {
-    number: number,
-    is_prime: numberIsPrime(number),
-    is_perfect: numberIsPerfect(number),
-    properties: numberProperties,
-    digit_sum: sumOfDigits(number),
-    fun_fact: funFact,
-  };
+      number: number,
+      is_prime: numberIsPrime(number),
+      is_perfect: numberIsPerfect(number),
+      properties: numberProperties,
+      digit_sum: sumOfDigits(number),
+      fun_fact: funFact,
+    };
 
-  res.status(200).json(resData);
-    
+    res.status(200).json(resData);
   } catch (error) {
     res.json({ error: "failed to fetch fun fact" });
   }
-
-
 });
 
 app.listen(PORT, () => {
